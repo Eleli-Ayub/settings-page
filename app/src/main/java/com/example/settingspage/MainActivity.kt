1bcf92cf3d914440451d5e347da0dcdf65086127
Eleli-Ayub <lesosioayub@gmail.com>
2023-08-28 22:24:10 +0300

    Started and finished the setting view in dark and light mode

---
+++ app/src/main/java/com/example/settingspage/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.settingspage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
